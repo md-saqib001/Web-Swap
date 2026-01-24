890f26b14a0385b1449a18543c88c1429210b2e9
mdsaqib <mdsaqib786bro@gmail.com>
2026-01-24 18:39:32 +0000

    Initialize basic Express server

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const PORT=3000;
+
+app.get('/', (req, res) => {
+    res.send("Hello! the server is running.");
+})
+
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
